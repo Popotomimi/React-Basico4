7e5bf8d18ce879ce0f85475009aceafaf620367d
Popotomimi <92410083+Popotomimi@users.noreply.github.com>
2023-05-25 20:46:55 -0300

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
+  const n = 15;
+
+  const redTitle = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* CSS Global */}
+      <h1>React com CSS</h1>
+      <div>
+        {/* CSS de componente */}
+        <MyComponent/>
+      </div>
+      <p>Este paragrafo é do App.js</p>
+      <p className="my-comp-paragraph">Este tbm é do componente</p>
+      {/* Inline CSS */}
+      <p style={{color: "blue", padding: "25px", backgroundColor: "yellow"}}>
+        Este elemento foi estilizado de forma inline.
+      </p>
+      {/* CSS Inline style Dinâmico */}
+      <p style={n < 10 ? ({color:"purple"}) : ({color: "brown"})}>
+        CSS Dinâmico
+      </p>
+      {/* Classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "black-title" }>
+      Este titulo vai ter classe dinâmica
+      </h2>
+      {/* CSS Modules */}
+      <Title />
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import './MyCompponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Css de componente</h1>
+        <p>Esse é o parágrafo do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent;
